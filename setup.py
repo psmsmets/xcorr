a22bd849f16e4f45cb1e50b30cd8ec53a0913901
Pieter Smets <mail@pietersmets.be>
2020-09-09 18:06:00 +0200

    entrypoints

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-           f'{NAME}-snr=scripts.snr:main',
-           f'{NAME}-psd=scripts.psd:main',
-           f'{NAME}-timelapse=scripts.timelapse:main',
+           f'{NAME}-snr=xcorr.scripts.snr:main',
+           f'{NAME}-psd=xcorr.scripts.psd:main',
+           f'{NAME}-timelapse=xcorr.scripts.timelapse:main',
         ],
     },
     scripts=[],
