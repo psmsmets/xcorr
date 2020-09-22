814e09926e77556e998d1afdb80d127847422997
Pieter Smets <mail@pietersmets.be>
2020-09-22 22:27:45 +0200

    Least-squares estimate plane wave

---
+++ xcorr/signal/plane_wave.py
@@ -0,0 +1,148 @@
+r"""
+
+:mod:`signal.plane_wave` -- Plane wave
+======================================
+
+Least squares estimation of a plane wave to an N-D labeled array of data.
+
+"""
+
+
+# Mandatory imports
+import xarray as xr
+import numpy as np
+
+# Relative imports
+from ..signal.correlate import correlate1d
+
+
+__all__ = ['plane_wave']
+
+
+def plane_wave(
+    s: xr.DataArray, x: xr.DataArray, y: xr.DataArray, dim: str = None
+):
+    """
+    Return the estimted plane wave given a signal and xy-coordinates,
+    all N-D labeled data arrays.
+
+    Parameters
+    ----------
+    s : :class:`xarray.DataArray`
+        The signal data array to estimate the plane wave.
+
+    x : :class:`xarray.DataArray`
+        The x-coordinate data array. Should be one-dimensional.
+
+    y : :class:`xarray.DataArray`
+        The y-coordaine data array. Should be one-dimensional.
+
+    dim : `str`, optional
+        The coordinate name of ``s`` defining the signal time dimension.
+        Defaults to the last dimension of ``s``.
+        Other dimensions (excluding the xy-coordinate dimension) will be
+        broadcasted.
+
+    Returns
+    -------
+
+    doa : :class:`xarray.DataArray`
+        The direction of arrival (back-azimuth, clockwise from north).
+
+    vel : :class:`xarray.DataArray`
+        The horizontal propagation velocity across the array.
+
+    """
+    # signal dim
+    dim = dim or s.dims[-1]
+    if not isinstance(dim, str):
+        raise TypeError('dim should be a string')
+    if dim not in s.dims:
+        raise ValueError(f's has no dimensions "{dim}"')
+
+    # x-coordinate
+    if len(x.dims) != 1:
+        raise ValueError('x should be a coordinate or a variable with '
+                         'a single dimension!')
+    rdim = x.dims[0]
+    r = s[rdim].drop_vars(tuple(d for d in s[rdim].coords if d != rdim))
+    if rdim not in s.dims:
+        raise ValueError(f's has no dimension "{x.dims[0]}"')
+    if not x[rdim].equals(r):
+        raise ValueError(f'dimension "{rdim}" of s and x do not agree')
+
+    # y-coordinate
+    if len(y.dims) != 1:
+        raise ValueError('y should be a coordinate or a variable with '
+                         'a single dimension!')
+    if not y[rdim].equals(r):
+        raise ValueError(f'dimension "{rdim}" of s and y do not agree')
+
+    # output dims
+    out_dims = tuple(c for c in s.coords if c not in (dim, rdim))
+    out_shape = tuple(s[c].size for c in out_dims)
+
+    # construct co-array receiver couples
+    N = r.size
+    coAi0, coAi1 = np.triu_indices(N, 1)
+    M = coAi0.size
+
+    # construct location matrix
+    A = np.array((
+        x.isel({rdim: coAi1}).values - x.isel({rdim: coAi0}).values,
+        y.isel({rdim: coAi1}).values - y.isel({rdim: coAi0}).values,
+    )).T
+
+    # prepare LSE
+    ATAinvAT = np.linalg.inv(A.T.dot(A)).dot(A.T)
+
+    # obtain lag times per co-array receiver couple
+    tau = xr.DataArray(
+        data=np.zeros(out_shape + (M,), dtype=x.dtype),
+        dims=out_dims + ('M',),
+        coords={d: s[d] for d in out_dims},
+    )
+    for i in range(M):
+        cc = correlate1d(
+            in1=s.isel({rdim: coAi0[i]}),
+            in2=s.isel({rdim: coAi1[i]}),
+            dim=dim
+        )
+        argmax = np.abs(cc).argmax(dim='delta_'+dim)
+        tau.loc[{'M': i}] = cc['delta_'+dim][argmax]
+
+    # estimate plane wave (broadcast-like)
+    def LSE(tau):
+        s = ATAinvAT.dot(tau)
+        vel = 1/np.linalg.norm(s)
+        doa = (np.arctan2(s[0], s[1]).item() * 180./np.pi) % 360.
+        return doa, vel
+    av = np.apply_along_axis(LSE, -1, tau.values)
+
+    # output
+    doa = xr.DataArray(
+        data=np.take(av, 0, axis=-1),
+        dims=out_dims,
+        coords={d: s[d] for d in out_dims},
+        attrs={
+            'long_name': 'Direction of arrival',
+            'standard_name': 'direction_of_arrival',
+            'units': 'degrees',
+            'reference': 'clockwise from north',
+        },
+        name='doa',
+    )
+
+    vel = xr.DataArray(
+        data=np.take(av, 1, axis=-1),
+        dims=out_dims,
+        coords={d: s[d] for d in out_dims},
+        attrs={
+            'long_name': 'Horizontal velocity',
+            'standard_name': 'horizontal_velocity',
+            'units': 'm s-1',
+        },
+        name='vel',
+    )
+
+    return doa, vel
