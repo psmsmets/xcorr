c403d9629597a022626417ef7d5c2c7291dbabdc
Pieter Smets <mail@pietersmets.be>
2020-09-24 15:05:42 +0200

    compute fix

--- xcorr/scripts/timelapse.py
+++ xcorr/scripts/timelapse.py
@@ -338,16 +338,10 @@ def process_spectrogram_timelapse(
         correlate_spectrograms,
         args=[root],
         template=ds,
-    ).persist()
-
-    # force await on async
-    distributed.wait(mapped)
-
-    # load blocks
-    ds = client.gather(mapped).load()
+    )
 
     # compute blocks
-    # ds = mapped.compute()
+    ds = mapped.compute()
 
     # fill upper triangle
     if sparse:
