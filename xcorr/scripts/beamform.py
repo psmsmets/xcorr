6b3237c3b5821609e40464f1308ce5e99d04bd6e
Pieter Smets <mail@pietersmets.be>
2020-09-25 13:53:00 +0200

    bugfix

--- xcorr/scripts/beamform.py
+++ xcorr/scripts/beamform.py
@@ -91,7 +91,7 @@ def plane_waves_list(xy, start, end, root):
     """
     results = []
     for day in pd.date_range(start, end, freq='1D'):
-        ds = load(xy.pair, time, root)
+        ds = load(xy.pair, day, root)
         cc = process(ds)
         fit = lse_fit(cc, xy)
         results.append(fit)
